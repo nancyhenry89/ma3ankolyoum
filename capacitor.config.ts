84a76f625062c99b99049e1bd1456e38711641c3
Nancy Henry <nancy@teamcroco.com>
2025-12-15 14:33:03 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ma3an-kol-youm',
+  webDir: 'dist'
+};
+
+export default config;
