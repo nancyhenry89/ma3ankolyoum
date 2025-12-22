15859771c4b8f21e5b9c49485f1f7eeaac09b8ee
nancyhenry89 <nancyhenry89@gmail.com>
2025-12-22 21:10:33 +0200

    fix web

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,21 @@
-/// <reference types="vitest" />
-
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/ma3ankolyoum/', // ✅ مهم لـ GitHub Pages
+  base: '/ma3ankolyoum/',
 
-  plugins: [
-    vue(),
-    legacy()
-  ],
+  plugins: [vue(), legacy()],
+
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: { '@': path.resolve(__dirname, './src') },
   },
 
-  // ✅ مهم: اعتبر mp3 assets عشان تتضمن في build
   assetsInclude: ['**/*.mp3'],
 
-  test: {
-    globals: true,
-    environment: 'jsdom'
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
   }
 })
