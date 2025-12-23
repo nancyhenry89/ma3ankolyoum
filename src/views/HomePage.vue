6ee003dd4c12869be9c3bcdfbcef38badb7f64a0
nancyhenry89 <nancyhenry89@gmail.com>
2025-12-23 19:19:12 +0200

    test content url

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -424,9 +424,11 @@ type ChapterPreview = {
   chapterTitle: string
   sections: { title: string }[]
 }
+
+
 const CONTENT_BASE = Capacitor.isNativePlatform()
   ? 'https://nancyhenry89.github.io/ma3ankolyoum/src/content'
-  : `${import.meta.env.BASE_URL}content`
+  : 'https://ma3ankolyoum.org/content'
   const AGBIA_AUDIO_BASE = `${CONTENT_BASE}/audio/agbia`
 
 const router = useRouter()
