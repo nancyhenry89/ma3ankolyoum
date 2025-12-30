f063adebb14c9d2dabaed0bb6dfc855dbd32c24b
nancyhenry89 <nancyhenry89@gmail.com>
2025-12-30 13:36:35 +0200

    new ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: "com.nancyhenry.ma3ankolyoum",
   appName: 'معاً كل يوم',
-  webDir: 'docs'
+  webDir: 'docs',
+  bundledWebRuntime: false
 };
 
 export default config;
