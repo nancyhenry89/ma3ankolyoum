737fdfc4962548ed6996cab9be00f70b72b1fa89
nancyhenry89 <nancyhenry89@gmail.com>
2026-01-05 13:22:41 +0200

    Jan 6 coptic

--- docs/assets/md.transition-legacy-438Ntv9n.js
+++ docs/assets/md.transition-legacy-438Ntv9n.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CrbaEix_.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.A}],execute:function(){
+System.register(["./index-legacy-mtDb25-X.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.A}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
