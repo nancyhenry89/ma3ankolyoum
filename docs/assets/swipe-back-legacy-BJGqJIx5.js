2a08bf1066b7d7d2cab3e41ae1209061fc05bd41
nancyhenry89 <nancyhenry89@gmail.com>
2026-01-21 14:57:26 +0200

    test features

---
+++ docs/assets/swipe-back-legacy-BJGqJIx5.js
@@ -0,0 +1,5 @@
+System.register(["./index-legacy-BNzjmLgh.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.ab,r=t.ac,i=t.ad}],execute:function(){
+/*!
+             * (C) Ionic http://ionicframework.com - MIT License
+             */
+t("createSwipeBackGesture",(t,e,s,a,c)=>{const o=t.ownerDocument.defaultView;let u=n(t);const d=t=>u?-t.deltaX:t.deltaX;return r({el:t,gestureName:"goback-swipe",gesturePriority:101,threshold:10,canStart:r=>(u=n(t),(t=>{const{startX:e}=t;return u?e>=o.innerWidth-50:e<=50})(r)&&e()),onStart:s,onMove:t=>{const e=d(t)/o.innerWidth;a(e)},onEnd:t=>{const e=d(t),n=o.innerWidth,r=e/n,s=(t=>u?-t.velocityX:t.velocityX)(t),a=s>=0&&(s>.2||e>n/2),l=(a?1-r:r)*n;let h=0;if(l>5){const t=l/Math.abs(s);h=Math.min(t,540)}c(a,r<=0?.01:i(0,r,.9999),h)}})})}}});
