b2ea9f5944aac934eccef99ee9a593c2191eb064
nancyhenry89 <nancyhenry89@gmail.com>
2026-01-30 14:26:28 +0200

    coptic word

---
+++ docs/assets/web-BMWQkZ8Y.js
@@ -0,0 +1,1 @@
+import{a8 as n}from"./index-TY8fy2HO.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
