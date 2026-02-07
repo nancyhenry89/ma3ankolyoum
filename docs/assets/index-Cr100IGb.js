ac5fa4efe4dbbda69c186974a343b01359adcf41
nancyhenry89 <nancyhenry89@gmail.com>
2026-02-07 15:23:49 +0200

    update

--- docs/assets/index-Cr100IGb.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CjsVIm-7.js","assets/index-CSVLPm7o.js","assets/index-BkH2RQ8L.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as o}from"./index-CSVLPm7o.js";const t=e("Browser",{web:()=>o(()=>import("./web-CjsVIm-7.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
