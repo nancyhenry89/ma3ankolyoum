23966a704060fecd3190f619081dcf7b830320ac
nancyhenry89 <nancyhenry89@gmail.com>
2026-02-08 11:26:21 +0200

    content

---
+++ docs/assets/index-ORJDj_np.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DrHBCeeu.js","assets/index-CtYoeSRt.js","assets/index-DxIaVw5c.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-CtYoeSRt.js";const t=e("Browser",{web:()=>o(()=>import("./web-DrHBCeeu.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
---
+++ docs/assets/index-legacy-Bvh8aMPh.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-DkQDwIP6.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.r,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-Cwq0Vhng.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
