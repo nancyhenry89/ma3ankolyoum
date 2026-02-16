4f6d743e012ddfb601a06571abce9b204d49aa40
nancyhenry89 <nancyhenry89@gmail.com>
2026-02-16 16:47:00 +0200

    data

--- docs/assets/web-DgrEX0i4.js
+++
@@ -1,1 +0,0 @@
-import{ay as n}from"./index-B_3ioKHg.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
