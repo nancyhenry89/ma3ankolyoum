06bc97b560ebd805628094bb8076efbbca040740
nancyhenry89 <nancyhenry89@gmail.com>
2026-02-17 18:38:25 +0200

    content

---
+++ docs/assets/index-CNvuT09o.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-SUHTNBG-.js","assets/index-Dy2hHSGP.js","assets/index-CG4JgpMP.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-Dy2hHSGP.js";const t=e("Browser",{web:()=>o(()=>import("./web-SUHTNBG-.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
