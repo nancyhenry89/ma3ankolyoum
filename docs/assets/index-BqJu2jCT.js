42950633a108cf695e40427660d2cf6ea058dee2
nancyhenry89 <nancyhenry89@gmail.com>
2026-02-22 22:14:06 +0200

    coptic word

---
+++ docs/assets/index-BqJu2jCT.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BwwahBFP.js","assets/index-B77egwxv.js","assets/index-CG4JgpMP.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-B77egwxv.js";const t=e("Browser",{web:()=>o(()=>import("./web-BwwahBFP.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
---
+++ docs/assets/index-legacy-Dc6atDif.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-DrgZRPeC.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.r,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-DoHG9H9i.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
