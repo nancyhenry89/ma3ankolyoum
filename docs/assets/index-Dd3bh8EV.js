3ace0a0962c039bd7be26cd67b20994483f273f7
nancyhenry89 <nancyhenry89@gmail.com>
2026-03-05 09:44:52 +0200

    test

--- docs/assets/index-Dd3bh8EV.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BwnXYRL7.js","assets/index-2Xruo6CR.js","assets/index-Chu711db.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as o}from"./index-2Xruo6CR.js";const t=e("Browser",{web:()=>o(()=>import("./web-BwnXYRL7.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
