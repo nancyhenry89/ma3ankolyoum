f6ae1645e9e5282f29ceab9cb917ca1be60b9bc3
nancyhenry89 <nancyhenry89@gmail.com>
2026-03-08 10:32:48 +0200

    content

--- docs/assets/index-legacy-B7E4sA0D.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-5Me4sZ1m.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.r,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-D0GVaipe.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
