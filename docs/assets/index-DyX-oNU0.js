dae2197ec89058baa224fd91a45c37a9cf97ab02
nancyhenry89 <nancyhenry89@gmail.com>
2026-03-18 19:25:11 +0200

    content

--- docs/assets/index-DyX-oNU0.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CByWMZx2.js","assets/index-CZEUDAyM.js","assets/index-BTfTgBVk.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as o}from"./index-CZEUDAyM.js";const t=e("Browser",{web:()=>o(()=>import("./web-CByWMZx2.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
