ce7ab3f3f5038f3f9ab2572e1651fe89210b01ac
nancyhenry89 <nancyhenry89@gmail.com>
2026-03-27 20:41:55 +0200

    content

--- docs/assets/index-legacy-Bm1_wsSJ.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-C3cyWXLi.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.q,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-zThIDBLK.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
---
+++ docs/assets/index-legacy-DykyX2fU.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-JjQUW6yW.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.q,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-BwEWJXZB.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
