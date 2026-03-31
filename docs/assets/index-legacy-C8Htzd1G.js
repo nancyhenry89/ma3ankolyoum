0250ebe7edd4e80f9c0d2f46a30231dd15b63434
nancyhenry89 <nancyhenry89@gmail.com>
2026-03-31 17:50:53 +0200

    coptic

--- docs/assets/index-legacy-C8Htzd1G.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-q0qmOnlD.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.q,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-C8dE-fSn.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
---
+++ docs/assets/index-legacy-INzSkq0N.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-D0QdyLr3.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.q,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-D2tC1NlL.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
