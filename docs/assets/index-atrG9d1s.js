4bf8f264934045416d4abd57107aa3693019023f
nancyhenry89 <nancyhenry89@gmail.com>
2026-04-06 09:18:01 +0200

    monday v2

--- docs/assets/index-atrG9d1s.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D55StFO8.js","assets/index-CLJZcehj.js","assets/index-Dg3VSSQx.css"])))=>i.map(i=>d[i]);
-import{q as e,_ as o}from"./index-CLJZcehj.js";const t=e("Browser",{web:()=>o(()=>import("./web-D55StFO8.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
