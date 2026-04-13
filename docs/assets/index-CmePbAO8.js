6ae170905a80ed501c64876b0569c686eef48b41
nancyhenry89 <nancyhenry89@gmail.com>
2026-04-13 18:41:57 +0200

    content

--- docs/assets/index-CmePbAO8.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Df19FZS-.js","assets/index-C24eZSr-.js","assets/index-Dg3VSSQx.css"])))=>i.map(i=>d[i]);
-import{q as e,_ as o}from"./index-C24eZSr-.js";const t=e("Browser",{web:()=>o(()=>import("./web-Df19FZS-.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
