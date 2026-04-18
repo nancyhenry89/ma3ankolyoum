08d6973281c31102174603a030115a433cdc8d09
nancyhenry89 <nancyhenry89@gmail.com>
2026-04-18 11:52:17 +0200

    content

--- docs/assets/index-legacy-VGuul4cj.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-C-uNsXWo.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.q,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-D8uM5KQ1.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
