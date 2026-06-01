fadd0e89f740d20193f4ed989deae6673638a4bd
nancyhenry89 <nancyhenry89@gmail.com>
2026-06-01 11:18:41 +0300

    content

--- docs/assets/index-legacy-CSzULs3-.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-CNohssJ1.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.q,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-CjH5dWfF.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
--- docs/assets/web-legacy-BbwHywe8.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-CNohssJ1.js"],function(e,n){"use strict";var r;return{setters:[e=>{r=e.aI}],execute:function(){e("AppLauncherWeb",class extends r{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}})}}});
