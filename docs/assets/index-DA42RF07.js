edfe027538206c86350d1bb007d85e4a6bd5c72f
nancyhenry89 <nancyhenry89@gmail.com>
2026-06-03 12:10:04 +0300

    content

---
+++ docs/assets/index-DA42RF07.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-EidUk8ro.js","assets/index-c3CSXC52.js","assets/index-Dg3VSSQx.css"])))=>i.map(i=>d[i]);
+import{m as e,_ as o}from"./index-c3CSXC52.js";const t=e("Browser",{web:()=>o(()=>import("./web-EidUk8ro.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
