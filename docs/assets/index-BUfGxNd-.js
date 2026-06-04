d81e061640fcb4bcc68df73c933b367dc6fd21b5
nancyhenry89 <nancyhenry89@gmail.com>
2026-06-04 13:56:57 +0300

    test

---
+++ docs/assets/index-BUfGxNd-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CkNyKrCa.js","assets/index-DJ04qb0v.js","assets/index-Dg3VSSQx.css"])))=>i.map(i=>d[i]);
+import{m as e,_ as o}from"./index-DJ04qb0v.js";const t=e("Browser",{web:()=>o(()=>import("./web-CkNyKrCa.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
--- docs/assets/status-tap-BJgd_pvi.js
+++
@@ -1,3 +0,0 @@
-import{aU as r,aV as i,aW as c,aX as d,aY as l}from"./index-DvHP5UaY.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
