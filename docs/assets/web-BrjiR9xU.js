1f278788d1ba7adeaedafa6650fd304d91701364
nancyhenry89 <nancyhenry89@gmail.com>
2026-06-05 22:17:49 +0300

    quiz online

---
+++ docs/assets/web-BrjiR9xU.js
@@ -0,0 +1,1 @@
+import{aZ as n}from"./index-oMlS0Six.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
