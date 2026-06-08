9a1b4445bb4c79f47ff24e497cb95fa6c38247d4
nancyhenry89 <nancyhenry89@gmail.com>
2026-06-08 19:38:17 +0300

    content + quiz css

---
+++ docs/assets/index-CFEviwBG.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BDHt7J-V.js","assets/index-DAvNp4v1.js","assets/index-DXFkNJP3.css"])))=>i.map(i=>d[i]);
+import{m as e,_ as o}from"./index-DAvNp4v1.js";const t=e("Browser",{web:()=>o(()=>import("./web-BDHt7J-V.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
--- docs/assets/web-C_Nt3e2p.js
+++
@@ -1,1 +0,0 @@
-import{aZ as n}from"./index-D0UFrtqP.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
