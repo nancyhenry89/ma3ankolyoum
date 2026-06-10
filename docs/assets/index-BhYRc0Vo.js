24574085c82df514fca5658f1786d1ed7eaba480
nancyhenry89 <nancyhenry89@gmail.com>
2026-06-10 10:21:04 +0300

    content

---
+++ docs/assets/index-BhYRc0Vo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-2DHnz-vL.js","assets/index-C8fttZL2.js","assets/index-DXFkNJP3.css"])))=>i.map(i=>d[i]);
+import{m as e,_ as o}from"./index-C8fttZL2.js";const t=e("Browser",{web:()=>o(()=>import("./web-2DHnz-vL.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
---
+++ docs/assets/web-C-8Ojytd.js
@@ -0,0 +1,1 @@
+import{aZ as n}from"./index-C8fttZL2.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
