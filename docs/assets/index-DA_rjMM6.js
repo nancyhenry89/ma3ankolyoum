a025a72096f03d28b772673108b6f70c6f607ecf
nancyhenry89 <nancyhenry89@gmail.com>
2026-06-30 11:11:07 +0300

    coptic till 5th of July

---
+++ docs/assets/index-DA_rjMM6.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C-QNlkoD.js","assets/index-B4bRafwv.js","assets/index-DXFkNJP3.css"])))=>i.map(i=>d[i]);
+import{m as e,_ as o}from"./index-B4bRafwv.js";const t=e("Browser",{web:()=>o(()=>import("./web-C-QNlkoD.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
---
+++ docs/assets/index-legacy-Dp-M1XtA.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-eX1kIuXF.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.m,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-DW8zEBea.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
