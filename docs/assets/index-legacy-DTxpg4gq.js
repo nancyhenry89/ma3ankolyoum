abe8387ffa531bd63d8eca2b38c83145fdc6273c
nancyhenry89 <nancyhenry89@gmail.com>
2026-08-05 20:38:47 +0300

    content

---
+++ docs/assets/index-legacy-DTxpg4gq.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-62ejoRc1.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.m,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-n7bsS8SA.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
--- docs/assets/index-vSojXmEW.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D4PYlH0J.js","assets/index-Y5xdAuV7.js","assets/index-DXFkNJP3.css"])))=>i.map(i=>d[i]);
-import{m as e,_ as o}from"./index-Y5xdAuV7.js";const t=e("Browser",{web:()=>o(()=>import("./web-D4PYlH0J.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
