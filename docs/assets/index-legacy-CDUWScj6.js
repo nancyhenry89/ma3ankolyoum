a26e50b61fdc35d9fa55d86d1823eabead8e5d75
nancyhenry89 <nancyhenry89@gmail.com>
2026-09-02 12:08:26 +0300

    content

--- docs/assets/index-legacy-CDUWScj6.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-Sk9Gh2rc.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.m,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-DT1khsM0.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
--- docs/assets/status-tap-legacy-BRt8wbyi.js
+++
@@ -1,5 +0,0 @@
-System.register(["./index-legacy-Sk9Gh2rc.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.bc,r=e.bd,s=e.be,o=e.bf,i=e.bg}],execute:function(){
-/*!
-             * (C) Ionic http://ionicframework.com - MIT License
-             */
-e("startStatusTap",()=>{const e=window;e.addEventListener("statusTap",()=>{n(()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=r(a);c&&new Promise(e=>s(c,e)).then(()=>{o(async()=>{c.style.setProperty("--overflow","hidden"),await i(c,300),c.style.removeProperty("--overflow")})})})})})}}});
