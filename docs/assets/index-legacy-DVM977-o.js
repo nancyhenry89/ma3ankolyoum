56cbc197e375f0b7fe1c754a172ec922a61506a0
nancyhenry89 <nancyhenry89@gmail.com>
2026-09-05 19:16:36 +0300

    content

---
+++ docs/assets/index-legacy-DVM977-o.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-BWR6PEin.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.m,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-BKkrYCGf.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
