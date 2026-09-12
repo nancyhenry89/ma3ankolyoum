9f3d8367ec946438045ddd66bc05655648a1aa04
nancyhenry89 <nancyhenry89@gmail.com>
2026-09-12 18:30:02 +0300

    content

--- docs/assets/index-legacy-DwDgLTuZ.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-Sq2zKL9F.js"],function(e,r){"use strict";var t,s;return{setters:[e=>{t=e.m,s=e._}],execute:function(){e("B",t("Browser",{web:()=>s(()=>r.import("./web-legacy-gQEgvlGy.js"),void 0).then(e=>new e.BrowserWeb)}))}}});
